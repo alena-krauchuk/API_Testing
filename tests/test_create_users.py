a0dc86cd87a9d3ef151e39b3f67126c87b7c97df
Elena <peregovorun@gmail.com>
2023-09-17 23:28:23 +0300

    feat: ref test_create_users.py

--- tests/test_create_users.py
+++ tests/test_create_users.py
@@ -9,22 +9,15 @@ class TestCreateUsers:
     }
 
     def test_create_user(self):
+        print(self.body.get("first_name"))
         response = MyRequests.post(url="/users/", data=self.body)
         print(response.json())
+        print(response.json()["first_name"])
+        print(response.status_code)
+        print(response.url)  # мое
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_get_user_first_name(self):
+        response = MyRequests.post(url="/users/", data=self.body)
+        print(response.json()["first_name"])
+        assert response.json()["first_name"] == "Jim"
+        # assert response.json()["first_name"] == self.body["first_name"]
