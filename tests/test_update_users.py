00cc7dfc6df4ff7818bc6454aa0a65148573fbae
Elena <peregovorun@gmail.com>
2023-09-19 22:42:27 +0300

    feat: ref test_update_users.py

--- tests/test_update_users.py
+++ tests/test_update_users.py
@@ -3,14 +3,22 @@ from src.my_requests import MyRequests
 
 class TestUpdateUsers:
     body = {
-        "first_name": "Jim",
-        "last_name": "Furry",
+        "first_name": "July",
+        "last_name": "Brando",
         "company_id": 2
     }
 
     def test_update_user(self):
         response = MyRequests.put(url="/users/22473", data=self.body)
         print(response.json())
+
+    # Проверить, что новое первое имя не равно предыдущему
+    def test_check_new_first_name_of_user(self):
+        response = MyRequests.put(url="/users/22473", data=self.body)
+        print(response.json()["first_name"])
+        assert response.json()["first_name"] != "Jim", "First name is incorrect"
+        # assert response.json()["first_name"] == self.body["first_name"], "First name is incorrect"
+
 
 
 
