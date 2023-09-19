1fa8d094ad617eed6718d3c43bdc5ad42c58ec7f
Elena <peregovorun@gmail.com>
2023-09-19 22:51:28 +0300

    feat: ref test_update_users.py

--- tests/test_update_users.py
+++ tests/test_update_users.py
@@ -12,6 +12,17 @@ class TestUpdateUsers:
         response = MyRequests.put(url="/users/22473", data=self.body)
         print(response.json())
 
+    # Можно сделать проверки id, статус-код при апдейте, имя новое с именем прежним, то же с фамилией и id компании
+
+    # __________________________________________________________________________________________________
+    # мой HW-тест 6
+    # Проверить, что id пользователя остался прежним
+    def test_check_user_id_is_the_same(self):
+        response = MyRequests.put(url="/users/22473", data=self.body)
+        print(response.json()["user_id"])
+        assert response.json()["user_id"] == 22473, "User id is not the same"
+
+    # мой HW-тест 7
     # Проверить, что новое первое имя не равно предыдущему
     def test_check_new_first_name_of_user(self):
         response = MyRequests.put(url="/users/22473", data=self.body)
