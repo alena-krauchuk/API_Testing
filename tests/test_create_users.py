be8a376e17052912d8a9c6d7501f2b141a542fda
Elena <peregovorun@gmail.com>
2023-09-19 21:31:18 +0300

    feat: ref test_create_users.py

--- tests/test_create_users.py
+++ tests/test_create_users.py
@@ -15,6 +15,11 @@ class TestCreateUsers:
         print(response.json()["first_name"])
         print(response.status_code)
 
+    def test_get_status_code_201(self):
+        response = MyRequests.post(url="/users/", data=self.body)
+        print(response.status_code)
+        assert response.status_code == 201, f"Status code isn't 201, status code is {response.status_code}"
+
     # мой HW-тест 1
     def test_get_user_url(self):
         response = MyRequests.post(url="/users/", data=self.body)
@@ -25,22 +30,22 @@ class TestCreateUsers:
     def test_get_user_first_name(self):
         response = MyRequests.post(url="/users/", data=self.body)
         print(response.json()["first_name"])
-        # assert response.json()["first_name"] == "Jim"
-        assert response.json()["first_name"] == self.body["first_name"]
+        # assert response.json()["first_name"] == "Jim", "First name is incorrect"
+        assert response.json()["first_name"] == self.body["first_name"], "First name is incorrect"
 
     # мой HW-тест 3
     def test_get_user_last_name(self):
         response = MyRequests.post(url="/users/", data=self.body)
         print(response.json()["last_name"])
-        assert response.json()["last_name"] == "Furry"
-        # assert response.json()["last_name"] == self.body["last_name"]
+        assert response.json()["last_name"] == "Furry", "Last name is incorrect"
+        # assert response.json()["last_name"] == self.body["last_name"], "Last name is incorrect"
 
     # мой HW-тест 4
     def test_get_user_company_id(self):
         response = MyRequests.post(url="/users/", data=self.body)
         print(response.json()["company_id"])
-        # assert response.json()["company_id"] == 1
-        assert response.json()["company_id"] == self.body["company_id"]
+        # assert response.json()["company_id"] == 1, "Company id is incorrect"
+        assert response.json()["company_id"] == self.body["company_id"], "Company id is incorrect"
 
     # мой HW-тест 5
     def test_get_user_id(self):
