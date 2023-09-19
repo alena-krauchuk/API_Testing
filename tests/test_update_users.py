f416f80acbf7008835a26623a1b19f6f3f973f75
Elena <peregovorun@gmail.com>
2023-09-19 22:56:54 +0300

    feat: ref test_update_users.py

--- tests/test_update_users.py
+++ tests/test_update_users.py
@@ -23,26 +23,24 @@ class TestUpdateUsers:
         assert response.json()["user_id"] == 22473, "User id is not the same"
 
     # мой HW-тест 7
-    # Проверить, что новое первое имя не равно предыдущему
+    # Проверить, что новое имя не равно предыдущему
     def test_check_new_first_name_of_user(self):
         response = MyRequests.put(url="/users/22473", data=self.body)
         print(response.json()["first_name"])
         assert response.json()["first_name"] != "Jim", "First name is incorrect"
         # assert response.json()["first_name"] == self.body["first_name"], "First name is incorrect"
 
+    # мой HW-тест 8
+    # Проверить, что новая фамилия не равна предыдущей
+    def test_check_new_last_name_of_user(self):
+        response = MyRequests.put(url="/users/22473", data=self.body)
+        print(response.json()["last_name"])
+        # assert response.json()["last_name"] != "Jim", "Last name is incorrect"
+        assert response.json()["last_name"] == self.body["last_name"], "Last name is incorrect"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # мой HW-тест 9
+    # Проверить статус-код апдейта
+    def test_get_status_code_201(self):
+        response = MyRequests.post(url="/users/22473", data=self.body)
+        print(response.status_code)
+        # assert response.status_code == 201, f"Status code isn't 201, status code is {response.status_code}"
