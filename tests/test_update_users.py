1f003be45352495900cb25e341c7d937bd254e4a
Elena <peregovorun@gmail.com>
2023-09-19 22:35:14 +0300

    feat: ref test_update_users.py

--- tests/test_update_users.py
+++ tests/test_update_users.py
@@ -5,14 +5,25 @@ class TestUpdateUsers:
     body = {
         "first_name": "Jim",
         "last_name": "Furry",
-        "company_id": 1
+        "company_id": 2
     }
 
     def test_update_user(self):
-        print(self.body.get("first_name"))
-        response = MyRequests.post(url="/users/", data=self.body)
+        response = MyRequests.put(url="/users/22473", data=self.body)
         print(response.json())
-        print(response.json()["first_name"])
-        print(response.status_code)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
