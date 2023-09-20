7e9fde3d733a7fabf4631c508b26fe2df218f86b
Elena <peregovorun@gmail.com>
2023-09-21 00:04:24 +0300

    feat: ref test_delete_users.py

--- tests/test_delete_users.py
+++ tests/test_delete_users.py
@@ -4,16 +4,21 @@ from src.my_requests import MyRequests
 class TestDeleteUsers:
 
     def test_delete_users(self):
-        response = MyRequests.delete(url="/users/22577")
+        response = MyRequests.delete(url="/users/22578")
         print(response.text)
         assert response.status_code == 202, f"Status code is not 202, status code is {response.status_code}"
 
+    def test_delete_users_has_text_null(self):
+        response = MyRequests.delete(url="/users/22583")
+        print(response.text)
+        assert response.text == "null", "Wrong text"
+
     def test_delete_deleted_users_has_status_code_404(self):
-        response = MyRequests.delete(url="/users/22576")
+        response = MyRequests.delete(url="/users/22578")
         assert response.status_code == 404, f"Status code is not 404, status code is {response.status_code}"
 
     def test_delete_deleted_users_has_text(self):
-        response = MyRequests.delete(url="/users/22576")
+        response = MyRequests.delete(url="/users/22578")
         assert response.json()["detail"]["reason"] == "User with requested id: 22576 is absent", "Wrong text"
 
 
