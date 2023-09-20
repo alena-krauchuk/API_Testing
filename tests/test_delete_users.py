5aeece31106da369cd21cdf852b0ca8a4a49f9eb
Elena <peregovorun@gmail.com>
2023-09-20 23:42:44 +0300

    feat: add test_delete_users.py

--- tests/test_delete_users.py
+++ tests/test_delete_users.py
@@ -4,9 +4,9 @@ from src.my_requests import MyRequests
 class TestDeleteUsers:
 
     def test_delete_users(self):
-        response = MyRequests.delete(url="/users/22573")
+        response = MyRequests.delete(url="/users/22574")
         print(response.json())
         print(response.status_code)
-        assert response.status_code == 202, "User id is not the same"
+        assert response.status_code == 202, f"Status code is not 202, status code is {response.status_code}"
 
 
